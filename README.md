da56e8997f0a5e10d0ec478f4010af6d16956962
aniketdibbe <aniket.dibbe@happiestminds.com>
2024-09-26 15:16:29 +0530

    routes api

---
+++ README.md
@@ -0,0 +1,9 @@
+To initialise the project
+-npm init
+-npm i express
+
+# What is dependancy?
+
+    -Any package on which ur project dependant on
+
+# Order of the route matters
--- src/app.js
+++ src/app.js
@@ -1,13 +1,22 @@
 const express = require("express");
 
 const app = express();
-app.use("/", (req, res) => {
-  res.send("Hello from home page!");
+
+app.get("/user", (req, res) => {
+  res.send({ Name: "Aniket" });
+});
+app.use("/test/abc", (req, res) => {
+  res.send("Hello world abc!");
 });
 
 app.use("/test", (req, res) => {
   res.send("Hello world!");
 });
+
+app.use("/", (req, res) => {
+  res.send("Hello from home page!");
+});
+
 app.listen(3000, () => {
   console.log("Server is Up!");
 });
